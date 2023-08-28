0b5d48117cd10149d6162a0d973f1825624729f5
kamran <kamranzafar128@gmail.com>
2023-08-28 15:21:40 +0500

    issues resolved

--- lib/helper/utils/service_locator_utils.dart
+++ lib/helper/utils/service_locator_utils.dart
@@ -1,15 +1,20 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:task_10/business_logic/bloc/main_screen_bloc/main_screen_bloc.dart';
 import 'package:task_10/repositories/alarm_repository.dart';
 import 'package:task_10/repositories/local_alarm_repo_impl.dart';
 
 import '../../business_logic/cubit/notifications_cubit/notifications_cubit.dart';
+import '../../data_provider/models/alarm_model/alarm_model.dart';
+import '../../data_provider/models/alarm_model/time_of_day_adapter.dart';
 import '../../main.dart';
+import '../constants/constants_resource.dart';
 import '../services/local_notifications_service/awesome_notification_services.dart';
 
 class ServiceLocator {
   ServiceLocator._();
 
-  static void registerDependencyContainer() {
+  static Future<void> registerDependencyContainer() async {
+    await hiveInit();
     getIt.registerLazySingleton<AlarmStorageRepository>(
         () => LocalAlarmRepoImpl(hiveAlarmBox: getIt()));
 
@@ -24,5 +29,13 @@ class ServiceLocator {
     );
     getIt.registerFactory<MainScreenBloc>(() => MainScreenBloc(
         alarmStorageRepository: getIt(), notificationsCubit: getIt()));
+  }
+
+  static Future hiveInit() async {
+    await Hive.initFlutter();
+    Hive.registerAdapter(AlarmModelAdapter());
+    Hive.registerAdapter(TimeOfDayAdapter());
+    final box = await Hive.openBox(ConstantsResource.HIVE_ALARM_BOX_NAME);
+    getIt.registerLazySingleton<Box>(() => box);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get_it/get_it.dart';
 import 'package:task_10/business_logic/bloc/main_screen_bloc/main_screen_bloc.dart';
 import 'package:task_10/business_logic/cubit/notifications_cubit/notifications_cubit.dart';
-import 'package:task_10/helper/utils/app_init_utils.dart';
 import 'package:task_10/presentation/screens/main_screen/main_screen.dart';
 
+import 'helper/services/local_notifications_service/awesome_notification_services.dart';
 import 'helper/theme/app_theme.dart';
+import 'helper/utils/service_locator_utils.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> main() async {
-  await AppInitUtils.appInit();
+  await appInit();
   runApp(const MyApp());
 }
 
@@ -39,4 +41,12 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> appInit() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  final notificationService = AwesomeNotificationsService();
+  await notificationService.init();
+  await ServiceLocator.registerDependencyContainer();
 }
